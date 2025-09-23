878e489d6f5991686652c47993ee7c3f0e1d97b8
Jabberrock <jabberrock@gmail.com>
2025-09-22 22:02:18 -0700

    Change dev port to 8633

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/slimevr-or-vive/'
+  base: '/slimevr-or-vive/',
+  server: {
+    port: 8633
+  }
 })
