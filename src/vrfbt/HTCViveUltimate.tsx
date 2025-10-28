6844dcd34b52665fb03759692d12961c8a5e9f2c
Jabberrock <jabberrock@gmail.com>
2025-10-27 23:26:55 -0700

    Fix ultimate affordability

--- src/vrfbt/HTCViveUltimate.tsx
+++ src/vrfbt/HTCViveUltimate.tsx
@@ -174,7 +174,7 @@ export function makeHTCViveUltimate(vrSystem: VRSystem, fbtConfigKey: FBTSystemC
         review: {
             cost: {
                 score: 2,
-                content: <p>You will need to buy the trackers, and straps to mount the trackers to your body.</p>,
+                content: <p>You will need to buy the trackers and straps.</p>,
             },
             setup: {
                 score: 4,
